08607d73417d1f6580ca254d2c9742c6169b0e43
akhileshwar <akhileshwar.rachakonda1@wipro.com>
2018-06-02 19:06:28 +0530

    refractored

---
+++ README.md
@@ -0,0 +1,14 @@
+## iplT20 Web App
+
+
+
+### How to start running
+Run these commands
+
+To install the required dependencies
+
+	$ npm install
+
+To run the app
+
+	$ npm run start
---
+++ webclient/components/teamsInfo.js
@@ -0,0 +1,84 @@
+module.exports ={
+  "Kolkata Knight Riders":{
+    url:'https://upload.wikimedia.org/wikipedia/en/thumb/4/4c/Kolkata_Knight_Riders_Logo.svg/200px-Kolkata_Knight_Riders_Logo.svg.png',
+    ab:"KKR"
+  },
+  "Royal Challengers Bangalore":{
+    url:'https://upload.wikimedia.org/wikipedia/en/thumb/9/9a/Royal_Challengers_Bangalore_Logo_2016.svg/200px-Royal_Challengers_Bangalore_Logo_2016.svg.png',
+    ab:"RCB"
+  },
+  "Chennai Super Kings":{
+    url:'https://upload.wikimedia.org/wikipedia/en/thumb/2/2b/Chennai_Super_Kings_Logo.svg/200px-Chennai_Super_Kings_Logo.svg.png',
+    ab:"CSK"
+  },
+  "Kings XI Punjab":{
+    url:'https://upload.wikimedia.org/wikipedia/en/thumb/e/e7/Kings_XI_Punjab_logo.svg/200px-Kings_XI_Punjab_logo.svg.png',
+    ab:"KXIP"
+  },
+  "Rajasthan Royals":{
+    url:'https://upload.wikimedia.org/wikipedia/en/thumb/6/60/Rajasthan_Royals_Logo.svg/200px-Rajasthan_Royals_Logo.svg.png',
+    ab:"RR"
+  },
+  "Delhi Daredevils":{
+    url:'https://vignette.wikia.nocookie.net/logopedia/images/8/8a/Delhi_Daredevils.svg.png/revision/latest?cb=20180113221053',
+    ab:"DD"
+  },
+  "Mumbai Indians":{
+    url:'https://upload.wikimedia.org/wikipedia/en/thumb/c/cd/Mumbai_Indians_Logo.svg/200px-Mumbai_Indians_Logo.svg.png',
+    ab:"MI"
+  },
+  "Deccan Chargers":{
+    url:'https://upload.wikimedia.org/wikipedia/en/thumb/a/a6/HyderabadDeccanChargers.png/200px-HyderabadDeccanChargers.png',
+    ab:"DC"
+  },
+  "Kochi Tuskers Kerala":{
+    url:'https://upload.wikimedia.org/wikipedia/en/thumb/9/96/Kochi_Tuskers_Kerala_Logo.svg/200px-Kochi_Tuskers_Kerala_Logo.svg.png',
+    ab:"KTK"
+  },
+  "Pune Warriors":{
+    url:'https://upload.wikimedia.org/wikipedia/en/thumb/4/4a/Pune_Warriors_India_IPL_Logo.png/200px-Pune_Warriors_India_IPL_Logo.png',
+    ab:"PWI"
+  },
+  "Sunrisers Hyderabad":{
+    url:'https://upload.wikimedia.org/wikipedia/en/thumb/8/81/Sunrisers_Hyderabad.svg/200px-Sunrisers_Hyderabad.svg.png',
+    ab:"SRH"
+  },
+  "Rising Pune Supergiants":{
+    url:'https://upload.wikimedia.org/wikipedia/en/thumb/9/9a/Rising_Pune_Supergiant.png/200px-Rising_Pune_Supergiant.png',
+    ab:"RPS"
+  },
+  "Gujarat Lions":{
+    url:'https://upload.wikimedia.org/wikipedia/en/c/c4/Gujarat_Lions.png',
+    ab:"GL"
+  },
+'V Kohli': 'http://iplstatic.s3.amazonaws.com/players/210/164.png',
+'CH Gayle': 'http://iplstatic.s3.amazonaws.com/players/210/236.png',
+'GJ Maxwell': 'http://iplstatic.s3.amazonaws.com/players/210/282.png',
+'DA Warner': 'http://iplstatic.s3.amazonaws.com/players/210/170.png',
+// 'SL Malinga': 'http://iplstatic.s3.amazonaws.com/players/210/211.png',
+'B Kumar': 'http://iplstatic.s3.amazonaws.com/players/210/116.png',
+// 'A Zampa': 'http://iplstatic.s3.amazonaws.com/players/210/958.png',
+'Ab de Villiers': 'http://iplstatic.s3.amazonaws.com/players/210/233.png',
+'KH Pandya': 'http://iplstatic.s3.amazonaws.com/players/210/3183.png',
+'AD Russell': 'http://iplstatic.s3.amazonaws.com/players/210/177.png',
+'SP Narine': 'http://iplstatic.s3.amazonaws.com/players/210/203.png',
+'DJ Bravo': 'http://iplstatic.s3.amazonaws.com/players/210/25.png',
+'A Nehra': 'http://iplstatic.s3.amazonaws.com/players/210/115.png',
+// 'MC Henriques': 'http://iplstatic.s3.amazonaws.com/players/210/388.png',
+'J Yadav': 'http://iplstatic.s3.amazonaws.com/players/210/1740.png',
+'RV Uthappa': 'http://iplstatic.s3.amazonaws.com/players/210/127.png',
+// 'JP Faulkner': 'http://iplstatic.s3.amazonaws.com/players/210/193.png',
+'MM Sharma': 'http://iplstatic.s3.amazonaws.com/players/210/1107.png',
+'RA Jadeja': 'http://iplstatic.s3.amazonaws.com/players/210/9.png',
+'Ankit Sharma': 'http://iplstatic.s3.amazonaws.com/players/210/32.png',
+'Z Khan': 'http://iplstatic.s3.amazonaws.com/players/210/165.png',
+'Gurkeerat Singh': 'http://iplstatic.s3.amazonaws.com/players/210/253.png',
+'DS Kulkarni': 'http://iplstatic.s3.amazonaws.com/players/210/101.png',
+'I Sharma': 'http://iplstatic.s3.amazonaws.com/players/210/38.png',
+'KA Pollard': 'http://iplstatic.s3.amazonaws.com/players/210/210.png',
+'MK Pandey': 'http://iplstatic.s3.amazonaws.com/players/210/123.png',
+// 'SE Marsh': 'http://iplstatic.s3.amazonaws.com/players/210/191.png',
+'BB McCullum': 'http://iplstatic.s3.amazonaws.com/players/210/202.png',
+'missing': 'http://iplstatic.s3.amazonaws.com/players/210/Photo-Missing.png',
+'TT Logo': 'http://www.iplt20.com/resources/ver/i/stats-bgs/TT.png',
+}
